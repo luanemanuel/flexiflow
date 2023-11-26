1c19362f40533d96edad165abd5a4cf19fc870e0
Luan Emanuel da Silva <programar@luanemanuel.com.br>
2023-11-26 12:20:09 -0300

    Development and initialization of the builder

--- lib/src/widgets/flexiflow_builder.dart
+++ lib/src/widgets/flexiflow_builder.dart
@@ -1,11 +1,14 @@
+import 'package:flexiflow/src/util/flow_manager.dart';
 import 'package:flutter/widgets.dart';
 
 class FlexiFlow extends StatefulWidget {
   const FlexiFlow({
+    required this.designSize,
     required this.child,
     super.key,
   });
 
+  final Size designSize;
   final Widget child;
 
   @override
@@ -13,6 +16,23 @@ class FlexiFlow extends StatefulWidget {
 }
 
 class _FlexiFlowState extends State<FlexiFlow> {
+  late final FlowManager _flowManager;
+
+  @override
+  void initState() {
+    super.initState();
+    _flowManager = FlowManager(
+      designSize: widget.designSize,
+      context: context,
+    );
+  }
+
+  @override
+  void didUpdateWidget(FlexiFlow oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    _flowManager.updateContext(context);
+  }
+
   @override
   Widget build(BuildContext context) {
     return widget.child;
