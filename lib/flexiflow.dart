a57cae148596391ff99d663aceb55e186e1f5841
Luan Emanuel da Silva <programar@luanemanuel.com.br>
2023-11-26 16:00:35 -0300

    Exports new features

--- lib/flexiflow.dart
+++ lib/flexiflow.dart
@@ -1,4 +1,7 @@
 library flexiflow;
 
 export 'src/util/flex_extension.dart';
+export 'src/util/iflow.dart';
 export 'src/widgets/flex_builder.dart';
+export 'src/widgets/flow.dart';
+export 'src/widgets/flow_builder.dart';
